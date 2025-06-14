dfb7d7f80d22d83732895c1d3c9834ed75f385d9
Svetiashke <darker.coop@gmail.com>
2025-06-14 20:41:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function revealOnScrollOnce() {
+    const elements = document.querySelectorAll(
+      '.product-card, .product, .section-title, .sale-section, .hero-content, .per h1, .per p, .top-sales, .footer, .footer h2, .footer-links li, .social-icons a'
+    );
+
+    for (let el of elements) {
+      if (el.classList.contains('active')) continue; // якщо вже з'явився — пропускаємо
+
+      const windowHeight = window.innerHeight;
+      const elementTop = el.getBoundingClientRect().top;
+      const elementVisible = 100;
+
+      if (elementTop < windowHeight - elementVisible) {
+        el.classList.add('active');
+      }
+    }
+  }
+
+  window.addEventListener('scroll', revealOnScrollOnce);
+  document.addEventListener('DOMContentLoaded', revealOnScrollOnce);
